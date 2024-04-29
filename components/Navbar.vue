95c519cb1acaa78008ac1683c1bf88a4fcd4b35c
Jason Cameron <jasoncameron246@gmail.com>
2024-04-29 18:19:44 -0400

    initial project setup

---
+++ components/Navbar.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <header class="header-container">
+        <nav class="navbar container">
+            <img class="logo" src="~/assets/images/logo-v1.png" alt="Initium Digital logo.">
+            <ul>
+            <li>
+                <NuxtLink to="/">Home</NuxtLink>
+            </li>
+        </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+.header-container {
+    width: 100%;
+    background: var(--header-footer-color);
+
+    .navbar {
+        display: flex;
+        padding: 1em 2em;
+        justify-content: space-between;
+
+        .logo {
+            margin: .5rem;
+        }
+
+        ul, li {
+            margin: 0;
+            list-style: none;
+            padding: 0;
+        }
+    }
+
+
+
+    .navbar ul li a {
+        color: black;
+        text-decoration: none;
+        margin: auto ;
+    }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import '~/assets/css/main.css'
+</script>
+
+<template>
+    <main>
+        <Navbar />
+    </main>
+</template>
+
+<style scoped>
+
+</style>
